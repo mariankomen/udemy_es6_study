ee406c515fdc2f9fb1dbd4e68c14f96e8bc3ae40
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-25 21:04:08 +0200

    Animal.prototype.NAMEOFPROTOTYPE

--- prototypes/prototypes_ex.js
+++ prototypes/prototypes_ex.js
@@ -1,17 +1,13 @@
-const animals = {
-    say(){
-        console.log(this.name, 'say', this.voice)
-    }
+function Animal(name, voice) {
+    this.name = name;
+    this.voice = voice;
 }
 
-function CreateAnimal(name, voice){
-    let result = Object.create(animals);
-    result.name = name;
-    result.voice = voice;
-    return result;
+Animal.prototype.say = function (){
+    console.log(this.name, this.voice)
 }
+const dog = new Animal("Dog","Woof")
+const cat = new Animal("Cat","Meaw")
 
-const dog = CreateAnimal("Dog", "Woof");
-const cat = CreateAnimal("Cat", "meaw");
 dog.say();
 cat.say();
