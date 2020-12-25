2016a3134c33b5115a40e00a1ae4e7c1b34bf7db
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-25 20:46:45 +0200

    func prototype

--- prototypes/prototypes_ex.js
+++ prototypes/prototypes_ex.js
@@ -3,8 +3,15 @@ const animals = {
         console.log(this.name, 'say', this.voice)
     }
 }
-const dog = Object.create(animals);
-dog.name = "Dog";
-dog.voice = "Woof";
 
+function CreateAnimal(name, voice){
+    let result = Object.create(animals);
+    result.name = name;
+    result.voice = voice;
+    return result;
+}
+
+const dog = CreateAnimal("Dog", "Woof");
+const cat = CreateAnimal("Cat", "meaw");
 dog.say();
+cat.say();
