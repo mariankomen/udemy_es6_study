8b5faaf54156cc5c8c8a630b6a858d16404c6e24
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-26 11:16:01 +0200

    Promise then

--- promises/promise.js
+++ promises/promise.js
@@ -1,15 +1,41 @@
+//
+// setTimeout(()=> {
+//     console.log("Preparing data..")
+//
+//     const BackEndData = {
+//         port: 2003,
+//         server: "aws",
+//         status: "Working"
+//     }
+//     setTimeout(()=>{
+//         BackEndData.modified = true;
+//         console.log("Data recieved: ", BackEndData)
+//     },2000)
+// },2000)
 console.log("Request data: ...")
 
-setTimeout(()=> {
-    console.log("Preparing data..")
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Preparing data..")
+        const BackEndData = {
+            port: 2003,
+            server: "aws",
+            status: "Working"
+        }
+        resolve(BackEndData)
+    }, 2000)
+})
 
-    const BackEndData = {
-        port: 2003,
-        server: "aws",
-        status: "Working"
-    }
-    setTimeout(()=>{
-        BackEndData.modified = true;
-        console.log("Data recieved: ", BackEndData)
-    },2000)
-},2000)
+p.then(data => {
+    const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true;
+            resolve(data);
+        },2000)
+    })
+    p2.then(ClientData => {
+        console.log("Acces denied: ", ClientData)
+    })
+})
+
+
