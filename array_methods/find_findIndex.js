cbac5b61310cc577130ffc818ac772eb96451cf7
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-28 16:18:46 +0200

    If we need to search item with concret opportunities

---
+++ array_methods/find_findIndex.js
@@ -0,0 +1,14 @@
+let people = [
+    {name: "Marian", age: 16, budget: 6000},
+    {name: "Slavik", age: 43, budget: 50000},
+    {name: "Roman", age: 15, budget: 12000},
+    {name: "Vasia", age: 23, budget: 1200},
+    {name: "Volodymyr", age: 67, budget: 100000},
+]
+
+const j = people.find(person => person.name === "Marian")
+console.log(j.name, j.budget);
+
+
+const jIndex = people.findIndex(person => person.name === "Roman")
+console.log(`Roman's index is: ${jIndex}`);
