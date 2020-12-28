e12628557fda1c3fd0d8d6051f4d61a2d05e7b83
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-28 14:40:20 +0200

    If we need to know is it every item of arr = condition

---
+++ array_methods/every.js
@@ -0,0 +1,10 @@
+let arr_1 = ['a','csd','javascript','react.js','a','b']
+
+console.log(arr_1.every(test => test === 'js'))  //false
+
+let arr_2 = ['js','js','js','js','js','js']
+
+console.log(arr_2.every(test => test === 'js'))  //true
+
+//if we need to know is it all items of arr = question
+
